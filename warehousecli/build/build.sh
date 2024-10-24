edbc0eba6ba439b68d033af2e64995558eb3c7a3
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-10-24 07:47:11 +0000

    Change paths

--- warehousecli/build/build.sh
+++ warehousecli/build/build.sh
@@ -4,12 +4,12 @@ TAG_NAME="${1:-0.0.0}"
 TAG_NAME="${TAG_NAME#v}"
 project=warehousecli
 
-if [ -d dist/ ]; then rm -r dist/; fi
-mkdir dist/
+if [ -d warehousecli/dist/ ]; then rm -r warehousecli/dist/; fi
+mkdir warehousecli/dist/
 
-mkdir -p ./debian/DEBIAN
+mkdir -p ./warehousecli/debian/DEBIAN
 
-cat <<EOF > ./debian/DEBIAN/control
+cat <<EOF > ./warehousecli/debian/DEBIAN/control
 Package: warehousecli
 Version: ${TAG_NAME}
 Section: utils
@@ -19,12 +19,12 @@ Maintainer: André Baldeweg <a@baldeweg.org>
 Description: Warehouse CLI
 EOF
 
-mkdir -p ./debian/usr/bin
-cp ./warehousecli/warehousecli ./debian/usr/bin
+mkdir -p ./warehousecli/debian/usr/bin
+cp ./warehousecli/warehousecli ./warehousecli/debian/usr/bin
 
-find debian/ -type f -exec chmod 0644 {} \;
-find debian/ -type d -exec chmod 0755 {} \;
+find warehousecli/debian/ -type f -exec chmod 0644 {} \;
+find warehousecli/debian/ -type d -exec chmod 0755 {} \;
 
-chmod +x debian/usr/bin/warehousecli
+chmod +x warehousecli/debian/usr/bin/warehousecli
 
-fakeroot dpkg -b ./debian dist/${project}.deb
+fakeroot dpkg -b ./warehousecli/debian warehousecli/dist/${project}.deb
