73fa33e51fea3e19f169b9ed1d5536ec262e5b99
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-10-28 20:05:45 +0000

    Extend test

--- gateway/cover/cover_test.go
+++ gateway/cover/cover_test.go
@@ -22,41 +22,64 @@ func TestSaveCover(t *testing.T) {
 		SaveCover(c, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7")
 	})
 
-	imageData, err := os.ReadFile("test.jpg")
-	if err != nil {
-		t.Fatalf("Error reading image file: %v", err)
+	testCases := []struct {
+		imageName      string
+		expectedImages []string
+	}{
+		{"test.jpg", []string{
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-l.jpg",
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-m.jpg",
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-s.jpg",
+		}},
+		{"test.png", []string{
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-l.png",
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-m.png",
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-s.png",
+		}},
+		{"test.webp", []string{
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-l.webp",
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-m.webp",
+			"36ee6d5c-820b-4f0c-9637-73b63dacc2a7-s.webp",
+		}},
 	}
 
-	body := new(bytes.Buffer)
-	writer := multipart.NewWriter(body)
-	part, _ := writer.CreateFormFile("cover", "test.jpg")
-	_, _ = io.Copy(part, bytes.NewReader(imageData))
-	writer.Close()
+	for _, tc := range testCases {
+		t.Run(tc.imageName, func(t *testing.T) {
+			imageData, err := os.ReadFile(tc.imageName)
+			if err != nil {
+				t.Fatalf("Error reading image file: %v", err)
+			}
 
-	req, _ := http.NewRequest("POST", "/upload", body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
+			body := new(bytes.Buffer)
+			writer := multipart.NewWriter(body)
+			part, _ := writer.CreateFormFile("cover", tc.imageName)
+			_, _ = io.Copy(part, bytes.NewReader(imageData))
+			writer.Close()
 
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
+			req, _ := http.NewRequest("POST", "/upload", body)
+			req.Header.Set("Content-Type", writer.FormDataContentType())
 
-	assert.Equal(t, http.StatusOK, w.Code)
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, req)
 
-	currentDir, _ := os.Getwd()
-	expectedFilePaths := []string{
-		filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7-l.jpg"),
-		filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7-m.jpg"),
-		filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7-s.jpg"),
+			assert.Equal(t, http.StatusOK, w.Code)
+
+			currentDir, _ := os.Getwd()
+
+			for _, expectedImage := range tc.expectedImages {
+				expectedFilePath := filepath.Join(currentDir, uploadsDir, expectedImage)
+				if _, err := os.Stat(expectedFilePath); os.IsNotExist(err) {
+					t.Errorf("Expected file %s to exist", expectedFilePath)
+				} else {
+					os.Remove(expectedFilePath)
+				}
+			}
+
+			assert.NoFileExists(t, filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7.jpg"))
+			assert.NoFileExists(t, filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7.png"))
+			assert.NoFileExists(t, filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7.webp"))
+
+			os.RemoveAll(filepath.Join(currentDir, uploadsDir))
+		})
 	}
-
-	for _, expectedFilePath := range expectedFilePaths {
-		if _, err := os.Stat(expectedFilePath); os.IsNotExist(err) {
-			t.Errorf("Expected file %s to exist", expectedFilePath)
-		} else {
-			os.Remove(expectedFilePath)
-		}
-	}
-
-	assert.NoFileExists(t, filepath.Join(currentDir, uploadsDir, "36ee6d5c-820b-4f0c-9637-73b63dacc2a7.jpg"))
-
-	os.RemoveAll(filepath.Join(currentDir, uploadsDir))
 }
