df4e09ca68a88d5066146963182df1a3a5c70cd3
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-02-03 20:57:32 +0000

    Refactor log import functionality into a dedicated importer package

---
+++ logs_import/importer/importer.go
@@ -0,0 +1,48 @@
+package importer
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/abaldeweg/warehouse-server/logs_import/db"
+	"github.com/abaldeweg/warehouse-server/logs_import/parser"
+	"github.com/spf13/viper"
+)
+
+func ImportLogs() {
+	entries, err := parser.ReadLogEntries()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	db, err := db.NewDBHandler()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	for _, entry := range entries {
+		if !isBlocked(entry.RequestPath) {
+			if err := db.Add(entry); err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
+		}
+	}
+
+	fmt.Println("\033[32mLogs successfully imported!\033[0m")
+	os.Exit(0)
+}
+
+func isBlocked(host string) bool {
+	blocklist := viper.GetStringSlice("blocklist")
+	for _, blocked := range blocklist {
+		if host == blocked {
+			return true
+		}
+	}
+	return false
+}
--- logs_import/main.go
+++ logs_import/main.go
@@ -4,13 +4,10 @@ import (
 	"time"
 
 	"github.com/abaldeweg/warehouse-server/framework/config"
-	"github.com/abaldeweg/warehouse-server/logs_import/db"
-	"github.com/abaldeweg/warehouse-server/logs_import/parser"
+	"github.com/abaldeweg/warehouse-server/logs_import/importer"
 	"github.com/spf13/viper"
 
 	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
@@ -21,46 +18,9 @@ func main() {
 
 	fmt.Println("Blocklist:", viper.GetStringSlice("blocklist"))
 
-	go importLogs()
+	go importer.ImportLogs()
 
 	for {
 		time.Sleep(1 * time.Second)
 	}
-}
-
-func importLogs() {
-	entries, err := parser.ReadLogEntries()
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	db, err := db.NewDBHandler()
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-	defer db.Close()
-
-	for _, entry := range entries {
-		if !isBlocked(entry.RequestPath) {
-			if err := db.Add(entry); err != nil {
-				log.Println(err)
-				os.Exit(1)
-			}
-		}
-	}
-
-	fmt.Println("\033[32mLogs successfully imported!\033[0m")
-	os.Exit(0)
-}
-
-func isBlocked(host string) bool {
-	blocklist := viper.GetStringSlice("blocklist")
-	for _, blocked := range blocklist {
-		if host == blocked {
-			return true
-		}
-	}
-	return false
 }
