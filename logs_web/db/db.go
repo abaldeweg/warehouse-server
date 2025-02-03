dd06b11f5d968c3d8e9434d73ba74ff482acba8f
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-02-03 21:16:49 +0000

    Remove unused Get method for fetching log entries from the database

--- logs_web/db/db.go
+++ logs_web/db/db.go
@@ -2,8 +2,6 @@ package db
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/abaldeweg/warehouse-server/logs_web/entity"
 	"github.com/spf13/viper"
@@ -39,41 +37,6 @@ func NewDBHandler() (*DBHandler, error) {
 // Close closes the database connection.
 func (handler *DBHandler) Close() error {
 	return handler.client.Disconnect(context.TODO())
-}
-
-// Get fetches log entries from the database within a specified time range.
-func (handler *DBHandler) Get(from int, to int) ([]entity.LogEntry, error) {
-	fromTime, err := time.Parse("20060102", fmt.Sprintf("%d", from))
-	if err != nil {
-		return nil, err
-	}
-	toTime, err := time.Parse("20060102", fmt.Sprintf("%d", to))
-	if err != nil {
-		return nil, err
-	}
-	toTime = toTime.Add(23*time.Hour + 59*time.Minute + 59*time.Second)
-
-	filter := bson.M{
-		"time": bson.M{
-			"$gte": fromTime.Format(time.RFC3339),
-			"$lte": toTime.Format(time.RFC3339),
-		},
-	}
-	cursor, err := handler.collection.Find(context.TODO(), filter)
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(context.TODO())
-
-	var logs []entity.LogEntry
-	for cursor.Next(context.TODO()) {
-		var logEntry entity.LogEntry
-		if err := cursor.Decode(&logEntry); err != nil {
-			return nil, err
-		}
-		logs = append(logs, logEntry)
-	}
-	return logs, nil
 }
 
 // FindDemanded retrieves logs based on the provided filter.
