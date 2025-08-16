52a99407c31b0c1f3bc576fb10a02a4124112b82
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-08-16 12:33:09 +0000

    Add MailReservation field to Branch model

--- gateway/core/models/branch.go
+++ gateway/core/models/branch.go
@@ -6,15 +6,16 @@ import (
 
 // Branch represents a branch entity with various attributes.
 type Branch struct {
-	ID        uint    `json:"id" gorm:"primaryKey;autoIncrement;->"`
-	Name      string  `json:"name" validate:"required,max=255"`
-	Steps     float32 `json:"steps" validate:"gte=0.00" gorm:"default:0.00"`
-	Currency  string  `json:"currency" validate:"required,oneof=EUR USD" gorm:"default:'EUR'"`
-	Ordering  string  `json:"ordering" gorm:"type:text"`
-	Public    bool    `json:"public" gorm:"default:false"`
-	Pricelist string  `json:"pricelist" gorm:"type:text"`
-	Cart      bool    `json:"cart" gorm:"default:false"`
-	Content   string  `json:"content" gorm:"type:text"`
+	ID              uint    `json:"id" gorm:"primaryKey;autoIncrement;->"`
+	Name            string  `json:"name" validate:"required,max=255"`
+	Steps           float32 `json:"steps" validate:"gte=0.00" gorm:"default:0.00"`
+	Currency        string  `json:"currency" validate:"required,oneof=EUR USD" gorm:"default:'EUR'"`
+	Ordering        string  `json:"ordering" gorm:"type:text"`
+	Public          bool    `json:"public" gorm:"default:false"`
+	Pricelist       string  `json:"pricelist" gorm:"type:text"`
+	Cart            bool    `json:"cart" gorm:"default:false"`
+	Content         string  `json:"content" gorm:"type:text"`
+	MailReservation string  `json:"mail_reservation" gorm:"type:text"`
 }
 
 // TableName returns the branch table name.
